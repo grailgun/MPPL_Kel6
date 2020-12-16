d0a65cbe23edce1866a22661e0c867d795596db6
0xff <rizqialus@gmail.com>
2020-12-17 00:54:04 +0700

    'Fix-controller-session'

--- app/Http/Controllers/PengusahaController.php
+++ app/Http/Controllers/PengusahaController.php
@@ -7,11 +7,12 @@ use Illuminate\Http\Request;
 
 class PengusahaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $pengusaha = Pengusaha::where('confirmed', 1)->paginate(6);
-        // $portofolio = Portofolio::All();
-        // dd($portofolio[0]->Galeries[0]->gambar);
+        
+        $request->session()->flush();
+        
         return view('index', compact('pengusaha'));
     }
 
